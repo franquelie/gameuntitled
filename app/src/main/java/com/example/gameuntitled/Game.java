053deaad62383eb53752a4ae81a2739df189e966
franquelie <franquelie@gmail.com>
2025-01-23 22:44:03 +0800

    Added stopLoop function

---
+++ app/src/main/java/com/example/gameuntitled/Game.java
@@ -0,0 +1,198 @@
+package com.example.gameuntitled;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import androidx.core.content.ContextCompat;
+
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import com.example.gameuntitled.gameobject.Circle;
+import com.example.gameuntitled.gameobject.Enemy;
+import com.example.gameuntitled.gameobject.Player;
+import com.example.gameuntitled.gameobject.Spell;
+import com.example.gameuntitled.gamepanel.Joystick;
+import com.example.gameuntitled.gamepanel.Performance;
+import com.example.gameuntitled.gamepanel.YouDied;
+import com.example.gameuntitled.graphics.SpriteSheet;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Game manages all objects in the game and is responsible for updating all states and render all
+ * objects to the screen
+ */
+class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private final Joystick joystick;
+    private final Player player;
+    private GameLoop gameLoop;
+    private List<Enemy> enemyList = new ArrayList<Enemy>();
+    private List<Spell> spellList = new ArrayList<Spell>();
+    private YouDied youDied;
+    private Performance performance;
+    private GameDisplay gameDisplay;
+
+    public Game(Context context) {
+        super(context);
+
+        // Get surface holder and add callback
+        SurfaceHolder surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        gameLoop = new GameLoop(this, surfaceHolder);
+
+        // Initialize game panels
+        performance = new Performance(context, gameLoop);
+        youDied = new YouDied(context);
+        joystick = new Joystick(275, 700, 70, 40);
+
+        // Initialize game objects
+        SpriteSheet spriteSheet = new SpriteSheet(context);
+        player = new Player(context, joystick, 2*500, 500, 32, spriteSheet.getPlayerSprite(0, 0, 64, 64));
+
+        // Initialize game display and center it around the player
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ((Activity) getContext()).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        gameDisplay = new GameDisplay(displayMetrics.widthPixels, displayMetrics.heightPixels, player);
+
+
+        setFocusable(true);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        // Handle touch event actions
+        // Revert without succeeding modification for event.getActionMasked()
+        // Ep. 08 - Spells and multi touch handling
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (joystick.getIsPressed()) {
+                    // Joystick was pressed before this event -> cast spell
+                    spellList.add(new Spell(getContext(), player));
+                } else if (joystick.isPressed((double) event.getX(), (double) event.getY())) {
+                    // Joystick is pressed in this event -> setIsPressed(true)
+                    joystick.setIsPressed(true);
+                } else {
+                    // Joystick was not pressed previously, and is not pressed in this event -> cast spell
+                    spellList.add(new Spell(getContext(), player));
+                }
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                // Joystick was pressed previously and is now moved
+                if(joystick.getIsPressed()) {
+                    joystick.setActuator((double) event.getX(), (double) event.getY());
+                }
+                return true;
+            case MotionEvent.ACTION_UP:
+                // Joystick was let go off -> setIsPressed(false) and resetActuator
+                joystick.setIsPressed(false);
+                joystick.resetActuator();
+                return true;
+        }
+
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        Log.d("Game.java", "surfaceCreated()");
+        if (gameLoop.getState().equals(Thread.State.TERMINATED)) {
+            gameLoop = new GameLoop(this, holder);
+        }
+        gameLoop.startLoop();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        Log.d("Game.java", "surfaceChanged()");
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        Log.d("Game.java", "surfaceDestroyed()");
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        // Draw game objects
+        player.draw(canvas, gameDisplay);
+
+        for (Enemy enemy : enemyList) {
+            enemy.draw(canvas, gameDisplay);
+        }
+
+        for (Spell spell : spellList) {
+            spell.draw(canvas, gameDisplay);
+        }
+
+        // Draw game panels
+        joystick.draw(canvas);
+        performance.draw(canvas);
+
+        // Draw You Died if HealthBar <= 0
+        if (player.getHealthPoints() <= 0) {
+            youDied.draw(canvas);
+        }
+
+    }
+
+    public void update() {
+        // Update game state
+        joystick.update();
+        player.update();
+
+        // Spawn enemy if it is time to spawn new enemies
+        if(Enemy.readyToSpawn()) {
+            enemyList.add(new Enemy(getContext(), player));
+        }
+
+        // Update states of each enemy
+        for (Enemy enemy : enemyList) {
+            enemy.update();
+        }
+
+        // Update states of each spell
+        for (Spell spell : spellList) {
+            spell.update();
+        }
+
+        // Iterate through enemyList and check for collision between each enemy and the player
+        // and all spells
+        Iterator<Enemy> iteratorEnemy = enemyList.iterator();
+        while (iteratorEnemy.hasNext()) {
+            Circle enemy = iteratorEnemy.next();
+            if (Circle.isColliding(enemy, player)) {
+                // Remove enemy if it collides with the player
+                iteratorEnemy.remove();
+                player.setHealthPoints(player.getHealthPoints() - 1);
+                continue;
+            }
+
+            Iterator<Spell> iteratorSpell = spellList.iterator();
+            while (iteratorSpell.hasNext()) {
+                Circle spell = iteratorSpell.next();
+                // remove spell if it collides with an enemy
+                if(Circle.isColliding(spell, enemy)) {
+                    iteratorSpell.remove();
+                    iteratorEnemy.remove();
+                    break;
+                }
+            }
+        }
+        gameDisplay.update();
+    }
+
+    public void pause() {
+        gameLoop.stopLoop();
+    }
+}
---
+++ app/src/main/java/com/example/gameuntitled/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.gameuntitled;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.Animator;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * MainActivity is the entry point to our application.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private Game game;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity.java", "onCreate()");
+        super.onCreate(savedInstanceState);
+
+        // Set window to fullscreen (will hide status bar)
+        /* Did not removed these because app stops working if removed */
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        // Set content view to game, so that objects in the Game class can be rendered to the screen
+        game = new Game(this);
+        setContentView(game);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("MainActivity.java", "onStart()");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("MainActivity.java", "onResume()");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("MainActivity.java", "onPause()");
+        game.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("MainActivity.java", "onStop()");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("MainActivity.java", "onDestroy()");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Log.d("MainActivity.java", "onBackPressed()");
+        // Comment out super to prevent any back press action
+        // super.onBackPressed();
+    }
+}
