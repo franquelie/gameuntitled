758f07b81a7285afe008deaa9f6de151afb7e5d9
franquelie <franquelie@gmail.com>
2025-01-23 22:54:15 +0800

    Added stopLoop function

---
+++ app/src/main/java/com/example/gameuntitled/GameDisplay.java
@@ -0,0 +1,37 @@
+package com.example.gameuntitled;
+
+import com.example.gameuntitled.gameobject.GameObject;
+
+public class GameDisplay {
+    private double gameToDisplayCoordinatesOffsetX;
+    private double gameToDisplayCoordinatesOffsetY;
+    private double displayCenterX;
+    private double displayCenterY;
+    private double gameCenterX;
+    private double gameCenterY;
+    private GameObject centerObject;
+
+    public GameDisplay(int widthPixels, int heightPixels, GameObject centerObject) {
+        this.centerObject = centerObject;
+
+        displayCenterX = widthPixels/2.0;
+        displayCenterY = heightPixels/2.0;
+    }
+
+
+    public void update() {
+        gameCenterX = centerObject.getPositionX();
+        gameCenterY = centerObject.getPositionY();
+
+        gameToDisplayCoordinatesOffsetX = displayCenterX - gameCenterX;
+        gameToDisplayCoordinatesOffsetY = displayCenterY - gameCenterY;
+    }
+
+    public double gameToDisplayCoordinatesX(double x) {
+        return x + gameToDisplayCoordinatesOffsetX;
+    }
+
+    public double gameToDisplayCoordinatesY(double y) {
+        return y + gameToDisplayCoordinatesOffsetY;
+    }
+}
---
+++ app/src/main/java/com/example/gameuntitled/gameobject/GameObject.java
@@ -0,0 +1,39 @@
+package com.example.gameuntitled.gameobject;
+
+import android.graphics.Canvas;
+
+import com.example.gameuntitled.GameDisplay;
+
+/**
+ * GameObject is an abstract class which is the foundation of all world objects in the game.
+ */
+public abstract class GameObject {
+    protected double positionX = 0;
+    protected double positionY = 0;
+    protected double velocityX = 0;
+    protected double velocityY = 0;
+    protected double directionX = 1;
+    protected double directionY = 0;
+
+    public GameObject(double positionX, double positionY) {
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+    public double getPositionX() { return positionX; }
+    public double getPositionY() { return positionY; }
+
+    protected double getDirectionX() { return directionX; }
+    protected double getDirectionY() { return directionY; }
+
+    public abstract void draw(Canvas canvas, GameDisplay gameDisplay);
+    public abstract void update();
+
+    protected static double getDistanceBetweenObjects(GameObject obj1, GameObject obj2) {
+        return Math.sqrt(
+                Math.pow(obj2.getPositionX() - obj1.getPositionX(), 2) +
+                Math.pow(obj2.getPositionY() - obj1.getPositionY(), 2)
+        );
+    }
+
+}
